6cd33f6e74d23c7d6a5f5d8f5ca662e6be862184
tomasbarron <jtomasbarron@gmail.com>
2021-03-08 16:47:17 -0300

    medida de FPS incorporados al modelo

--- model/yolo.py
+++ model/yolo.py
@@ -136,13 +136,26 @@ def detect_video(yolo, video_path, output_path=""):
     video_fps       = vid.get(cv2.CAP_PROP_FPS)
     video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
                         int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
- 
+    accum_time = 0
+    curr_fps = 0
+    fps = "FPS: ??"
+    prev_time = timer()
     while True:
         return_value, frame = vid.read()
         if not return_value:
             break
         image = Image.fromarray(frame)
         image = yolo.detect_image(image)
+        curr_time = timer()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 1
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            fps = "FPS: " + str(curr_fps)
+            curr_fps = 0
+        print(fps)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     contador = 1    
