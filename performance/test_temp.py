68c8ee614564dc52c636cd2411365cd0fd638280
tomasbarron <jtomasbarron@gmail.com>
2021-03-30 11:03:26 -0300

    modelo keras formateado

--- performance/test_temp.py
+++ performance/test_temp.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import sys
 sys.path.insert(0, '../model/keras')
-from yolo import YOLO, excecute
+from yolo import YOLO
 from timeit import default_timer as timer
 import numpy as np
 #archivo para pruebas
@@ -13,11 +13,19 @@ import numpy as np
 kerasModel = YOLO()
 print("empezo ejecucion de modelo")
 start = timer()
-resultado = excecute(kerasModel,'people3.mp4',"")
+resultado = kerasModel.analyze_video('people3.mp4')
 end = timer()
-meanPersons = np.mean(resultado['list'])
+meanPersons = np.mean(resultado)
 print('Rendimiento ----------')
 print('Personas encontradas en promedio por frame : ',meanPersons) 
 print("tiempo total : ",end-start)
-print("FPS promedio : ",((len(resultado['list']))/(end-start)))
+print("FPS promedio : ",((len(resultado))/(end-start)))
 print('-----------------------')
+
+
+'''funcion para imprimir json (se llama con -json_write(data))
+        def json_write(data):
+            #guardo datos en un archivo json
+            with open('{}.json'.format(video_path), 'w') as file:
+                json.dump(data, file, indent=4)
+'''
